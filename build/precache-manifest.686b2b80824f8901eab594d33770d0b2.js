7d6f584c0c7b38cb41ba78113c33fb99c324881e
SamVimes0 <k.aleksandr88@gmail.com>
2019-02-20 13:16:11 +0200

    gh-pages commit

---
+++ build/precache-manifest.686b2b80824f8901eab594d33770d0b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba0d4138eb7502646c1f",
+    "url": "/tic-tac-toe-react/build/static/css/main.9e09020e.chunk.css"
+  },
+  {
+    "revision": "ba0d4138eb7502646c1f",
+    "url": "/tic-tac-toe-react/build/static/js/main.ba0d4138.chunk.js"
+  },
+  {
+    "revision": "a0b101fe67937da93ab3",
+    "url": "/tic-tac-toe-react/build/static/js/runtime~main.a0b101fe.js"
+  },
+  {
+    "revision": "2a4449ffe270b058d27d",
+    "url": "/tic-tac-toe-react/build/static/js/2.2a4449ff.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe-react/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2e01c6e8524588ff8389e22429a87ae3",
+    "url": "/tic-tac-toe-react/build/index.html"
+  }
+];
---
+++ build/precache-manifest.e07e28cb3881102e986d935cfb9a9fd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e25d946ba6f651b4759",
+    "url": "/tic-tac-toe/build/static/css/main.376d070c.chunk.css"
+  },
+  {
+    "revision": "3e25d946ba6f651b4759",
+    "url": "/tic-tac-toe/build/static/js/main.3e25d946.chunk.js"
+  },
+  {
+    "revision": "7c74a0a18059363ec640",
+    "url": "/tic-tac-toe/build/static/js/runtime~main.7c74a0a1.js"
+  },
+  {
+    "revision": "2a4449ffe270b058d27d",
+    "url": "/tic-tac-toe/build/static/js/2.2a4449ff.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/tic-tac-toe/build/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "f839495b6508e0a82b5d2afdec4888d6",
+    "url": "/tic-tac-toe/build/index.html"
+  }
+];
